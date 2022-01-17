87f6623dea872dd1bd7a6df4dcdf4ab09744bc4e
Ifeanyi Okoloma <ifeanyiokoloma@gmail.com>
2022-01-17 17:45:01 +0100

    committed hero for the first time

---
+++ src/components/hero/Hero.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./hero.module.css";
+
+const Hero = () => {
+  return (
+    <div className={styles.hero}>
+      <picture>
+        <source
+          className={styles.img}
+          media="(max-width: 500px)"
+          srcset="./BGimages/computer-operator-mobile.jpg"
+        />
+        <source
+          className={styles.img}
+          media="(max-width: 800px)"
+          srcset="./BGimages/computer-operator-tablet.jpg"
+        />
+        <source
+          className={styles.img}
+          media="(max-width: 1024px)"
+          srcset="./BGimages/computer-operator-laptop.jpg"
+        />
+        <source
+          className={styles.img}
+          srcset="./BGimages/computer-operator-landscape.jpg"
+        />
+        <img
+          className={styles.img}
+          src="./BGimages/computer-operator-landscape.jpg"
+          alt="Computer Operator"
+        />
+      </picture>
+      <div className={styles.container}>
+        <div className={styles.text}>
+          <h1>Your Computer Support Center</h1>
+          <p>
+            CC7 Computers offers everything computer related: computer sales,
+            repair, maintenance, training and website creation and development.
+          </p>
+          <div className={styles.buttons}>
+            <button className={styles.shop}><Link to="/shop">shop</Link></button>
+            <button className={styles.contact}><Link to="/contact">contact us</Link></button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
