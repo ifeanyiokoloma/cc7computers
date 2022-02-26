ff82150cd8fa4a8ee4b4d0d4fcff50fa2a9d0e64
Ifeanyi Okoloma <ifeanyiokoloma@gmail.com>
2022-02-26 13:07:00 +0100

    second fix auth

--- src/components/ProtectAdmin.js
+++ src/components/ProtectAdmin.js
@@ -4,22 +4,20 @@ import useAuth from "../hooks/useAuth";
 const ProtectAdmin = () => {
   const location = useLocation();
 
-  const { loading, signIn, signOut, user } = useAuth();
+  const { loading, signIn, user } = useAuth();
 
   if (loading) {
     return <h1>Loading...</h1>;
-  }
-
-  if (
+  } else if (
     signIn &&
     (user.uid === "P7DQ5Ki2OBg1oQMQ0rR7DyEAM3G2" ||
       "bdp9PtF7LMhdFxDrkAGi2KwRSPS2")
   ) {
     return <Outlet />;
-  }
-
-  if (signOut) {
+  } else if (!signIn) {
     return <Navigate to="/login" state={{ from: location }} replace />;
+  } else {
+    return <h1>Sorry, you can't access this page</h1>;
   }
 };
 
--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,37 +1,41 @@
 import { onAuthStateChanged } from "firebase/auth";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { auth } from "../firebase/app";
 
 const useAuth = () => {
   const [loading, setLoading] = useState(true);
   const [signIn, setSignIn] = useState(false);
   const [signOut, setSignOut] = useState(false);
-  const [user, setUser] = useState({});
+  const [user, setUser] = useState("");
 
-  onAuthStateChanged(
-    auth,
-    (user) => {
-      if (user) {
-        // User is signed in, see docs for a list of available properties
-        // https://firebase.google.com/docs/reference/js/firebase.User
-        // const uid = user.uid;
-        setUser(user);
-        setSignIn(true);
-        setLoading(false);
-      } else {
-        // User is signed out
-        setSignIn(false);
-        setSignOut(true);
-        setLoading(false);
+  useEffect(() => {
+    var unSub = onAuthStateChanged(
+      auth,
+      (users) => {
+        if (users) {
+          // User is signed in, see docs for a list of available properties
+          // https://firebase.google.com/docs/reference/js/firebase.User
+          // const uid = user.uid;
+          setUser(users);
+          setSignIn(true);
+          setLoading(false);
+        } else {
+          // User is signed out
+          setSignIn(false);
+          setSignOut(true);
+          setLoading(false);
+        }
+      },
+      (error) => {
+        console.log(error);
+      },
+      (response) => {
+        console.log(response);
       }
-    },
-    (error) => {
-      console.log(error);
-    },
-    (response) => {
-      console.log(response);
-    }
-  );
+    );
+
+    return () => unSub();
+  }, [signIn]);
 
   return { loading, signIn, signOut, user };
 };
