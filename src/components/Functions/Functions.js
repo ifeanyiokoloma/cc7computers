d027b90b1d968ce685532eb22789ccd15ebf24f9
Ifeanyi Okoloma <ifeanyiokoloma@gmail.com>
2022-08-23 07:53:16 +0100

    added filter to functions

--- src/components/Functions/Functions.js
+++ src/components/Functions/Functions.js
@@ -5,3 +5,6 @@ import { Slide } from "@mui/material";
 export const Transition = forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
 });
+
+export const Filter = (products, id) =>
+  products.filter((product) => product.id === id);
