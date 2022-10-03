3d98418406e6082fc8a3d7a8a38518d2187d41f5
Ifeanyi Okoloma <ifeanyiokoloma@gmail.com>
2022-10-03 18:05:00 +0100

    Fixed About Page

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -1,64 +1,117 @@
 import Hero from "../../components/hero/Hero";
-import styles from "./about.module.css";
 // import Employees from "./Employees";
 import { about } from "../../data/articles/articles";
-import TypoBG from "../../components/TypoBG";
-import Article from "../../components/article/Article";
-import { IoTelescopeOutline } from "react-icons/io5";
-import { MdDirectionsCar } from "react-icons/md";
 import React from "react";
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  ListSubheader,
+  Stack,
+  Typography,
+} from "@mui/material";
+import { Container } from "@mui/system";
+import { StyledPaper } from "../../components/Styled/Styled";
+import { Circle } from "@mui/icons-material";
+import Team from "../../components/team/Team";
 
 const About = () => {
   return (
     <>
       <Hero page="about" imgName="keyboard" pageData={about} />
-      <TypoBG>
-        <section className={styles.container}>
-          <Article
-            title="Our Vision"
-            element="h2"
-            icon={<IoTelescopeOutline className={styles.icon} />}
-            textClass={styles.header}
-          >
-            <ul className={styles.visionList}>
-              <li>
-                To become the model for global computer systems support service
-                provision,
-              </li>
-              <li>
-                To ensure maximum productivity in the use of computer system,
-                leading to an enhanced perfomance of individuals and
-                organizations, as it relates to computing and business needs.
-              </li>
-            </ul>
-            <br />
-          </Article>
 
-          <Article
-            element="h2"
-            title="Our Mission"
-            icon={<MdDirectionsCar className={styles.icon} />}
-            textClass={styles.header}
-          >
-            <p className={styles.mission}>
-              Giving life and power to computing <br /> via heaven computing{" "}
-              <br />
-              (Gen 1:26, John 14:2)
-            </p>
-          </Article>
+      <Box my={5} component="main">
+        <Container maxWidth="sm">
+          <Stack spacing={5}>
+            <StyledPaper sx={{ p: 5 }}>
+              <List
+                subheader={
+                  <ListSubheader component="h2">Our Vision</ListSubheader>
+                }
+              >
+                <ListItem>
+                  <ListItemText>
+                    To become the model for global computer systems support
+                    service provision,
+                  </ListItemText>
+                </ListItem>
+                <ListItem>
+                  <ListItemText>
+                    To ensure maximum productivity in the use of computer
+                    system, leading to an enhanced perfomance of individuals and
+                    organizations, as it relates to computing and business
+                    needs.
+                  </ListItemText>
+                </ListItem>
+              </List>
+            </StyledPaper>
 
-          <Article element="h2" title="Our Values" textClass={styles.header}>
-            <ul>
-              <li>Wisdom</li>
-              <li>Integrity</li>
-              <li>Clarity</li>
-              <li>Accountability</li>
-              <li>Networking</li>
-            </ul>
-          </Article>
-        </section>
-      </TypoBG>
-      {/* <Employees /> */}
+            <StyledPaper sx={{ p: 5 }}>
+              <Box component="article">
+                <Stack spacing={2}>
+                  <Typography component="h2" variant="h5">
+                    Our Mission
+                  </Typography>
+                  <Typography component="p">
+                    Giving life and power to computing <br /> via heaven
+                    computing <br />
+                    (Gen 1:26, John 14:2)
+                  </Typography>
+                </Stack>
+              </Box>
+            </StyledPaper>
+
+            <StyledPaper sx={{ p: 5 }}>
+              <List
+                subheader={
+                  <ListSubheader component="h2">Our Values</ListSubheader>
+                }
+              >
+                <ListItem>
+                  <ListItemIcon>
+                    <Circle />
+                  </ListItemIcon>
+                  <ListItemText>Wisdom</ListItemText>
+                </ListItem>
+                <ListItem>
+                  <ListItemIcon>
+                    <Circle />
+                  </ListItemIcon>
+                  <ListItemText>Integrity</ListItemText>
+                </ListItem>
+                <ListItem>
+                  <ListItemIcon>
+                    <Circle />
+                  </ListItemIcon>
+                  <ListItemText>Clarity</ListItemText>
+                </ListItem>
+                <ListItem>
+                  <ListItemIcon>
+                    <Circle />
+                  </ListItemIcon>
+                  <ListItemText>Accountability</ListItemText>
+                </ListItem>
+                <ListItem>
+                  <ListItemIcon>
+                    <Circle />
+                  </ListItemIcon>
+                  <ListItemText>Networking</ListItemText>
+                </ListItem>
+              </List>
+            </StyledPaper>
+          </Stack>
+        </Container>
+        <Box mt={6}>
+          <Stack spacing={2} alignItems="center">
+            <Typography component="h2" variant="h5">
+              Our Team
+            </Typography>
+            <Team />
+          </Stack>
+        </Box>
+      </Box>
     </>
   );
 };
